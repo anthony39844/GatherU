8fcd9b745300ec6acd5c5149eb32c3585d2d37ec
chen7756 <chen7756@umn.edu>
2025-02-08 15:03:01 -0600

    ignore

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
+uri = "mongodb+srv://jchen012004:<db_password>@accounts.vumyj.mongodb.net/?retryWrites=true&w=majority&appName=Accounts"
+
 app = Flask(__name__)
