efc9d1225b69f6db49d1223074d960d84e5ce001
chen7756 <chen7756@umn.edu>
2025-02-08 15:12:47 -0600

    connect db

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,21 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://jchen012004:<db_password>@accounts.vumyj.mongodb.net/?retryWrites=true&w=majority&appName=Accounts"
+from dotenv import load_dotenv
+import os
+load_dotenv()
+database_password = os.getenv("DB_PASS")
+
+
+uri = f"mongodb+srv://jchen012004:{database_password}@accounts.vumyj.mongodb.net/?retryWrites=true&w=majority&appName=Accounts"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 app = Flask(__name__)
