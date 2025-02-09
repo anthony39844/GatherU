2f2a288169033901d1bfa3b0a0443f8b4976f91f
chen7756 <chen7756@umn.edu>
2025-02-08 18:01:06 -0600

    post events

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from bson import ObjectId
+from pydantic import BaseModel, EmailStr
 
 from dotenv import load_dotenv
 import os
@@ -14,6 +16,8 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
 db = client['test']
 user_collection = db['users']
+event_collection = db['events']
+
 
 try:
     client.admin.command('ping')
@@ -24,13 +28,20 @@ except Exception as e:
 
 app = FastAPI()
 
+class Account(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    contact: list
+    role: str = "user"
+    status: str = "unverified"
+
 #  ----- EVENT SHIT -----
 
 @app.get("/events")
 async def displayEvents():
-    collection = db['events']
 
-    res = collection.find()
+    res = event_collection.find()
     events = [event for event in res]
     for event in events:
         event["_id"] = str(event["_id"])
@@ -41,9 +52,12 @@ async def updateEvent():
 
     return {}
 
-@app.post("/events")
-async def createEvent():
-    
+@app.post("/add_events/")
+async def createEvent(item: dict):
+    org_id = str(user_collection.find({"username": item["org"]}).to_list()[0]['_id'])
+    new_item = {"name": item["name"], "time": item["time"],"place": item["place"],"org": item["org"],"org_id": org_id, "description": item["description"]}
+    event_id = str(event_collection.insert_one(new_item).inserted_id)
+    await updateAccount("events_list", event_id, org_id)
     return {}
 
 @app.delete("/events")
@@ -53,7 +67,7 @@ async def deleteEvent():
 
 # ----- ACCOUNT SHIT -----
 
-@app.get("/account")
+@app.get("/loginAccount")
 async def displayAccount():
     res = user_collection.find()
     accounts = [account for account in res]
@@ -61,17 +75,21 @@ async def displayAccount():
         account["_id"] = str(account["_id"])
     return accounts
 
-@app.patch("/account")
-async def updateAccount():
+@app.patch("/updateAccount")
+async def updateAccount(key, val, account_id):
+    if key == "events_list":
+        user_collection.update_one(
+            {'_id': ObjectId(account_id)},
+            {"$push": {key: val}});
 
     return {}
 
-@app.post("/account")
-async def createAccount():
-
+@app.post("/createAccount")
+async def createAccount(username, email, password, contact, events=[], role="user"):
+        
     return {}
 
-@app.delete("/account")
+@app.delete("/deleteaAccount")
 async def deleteAccount():
 
     return {}
